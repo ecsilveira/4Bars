f32af72f931a955481aee98a7842eebbe05653c1
ecsilveira <ecsilveira@gmail.com>
2015-09-01 23:41:49 -0700

    Signed-off-by: ecsilveira <ecsilveira@gmail.com>

--- BeerBars/src/Test.java
+++ BeerBars/src/Test.java
@@ -8,6 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// alterado cassio : chico alterou denovo
+		// alterado cassio : duda alterou de novo
 	}
 
 }
